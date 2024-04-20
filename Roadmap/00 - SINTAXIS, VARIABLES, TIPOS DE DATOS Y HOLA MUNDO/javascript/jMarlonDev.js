8b3a0b4d644a392694e51ae3acd53cae69defe6c
Jhon Marlon <jmarloncin07@gmail.com>
2024-04-20 12:32:21 -0500

    Primer ejercicio resuelto en Js soy nuevo

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/jMarlonDev.js
@@ -0,0 +1,46 @@
+
+/*curso de logica de programacion desarrollado por Moure dev Aqui estare 
+resolviendo los ejercicios planteados por el en la pagna oficial
+retos de programacion.com
+resolviendo el primer ejercicios #00 sintaxis, variables tipos de datos hola mundo
+
+// solucion
+
+//https://developer.mozilla.org/es/docs/Web/JavaScript 
+
+// primera forma de hacer un comentario este se llama comentario de una sola linea y se representa asi //
+
+/* esta es la segunda forma de hacer un comentario y se llama comentario
+de bloque el cual nos permite hacer un comentario de varias lineas
+se representa asi*/
+
+// variable
+let edad = 18;
+// constante
+const Pi = 3.141526;
+
+// representando los tipos de datos por medio de variables
+// strings (cadena de texto)
+let saludar = "Hola como estás como va todo";
+
+// enteros o numeros
+let numero1 = 1234;
+let float = 2.0933;
+let bigInt = 1234567890123456789n;
+
+// booleanos
+let numeroPar = true;
+let numeroImpar = false;
+
+let lenguaje = "JavaScript";
+
+// imprimiendo en pantalla el lenguaje de programación
+console.log("¡Hola, " + lenguaje + " preparado para la aventura!");
+
+console.log(typeof numero1);
+console.log(typeof bigInt)
+console.log(typeof float);
+console.log(typeof numeroPar);
+console.log(typeof numeroImpar);
+
+
